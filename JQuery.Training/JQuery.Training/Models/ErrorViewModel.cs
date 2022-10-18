83d325dc6af041201bd31047023dd944c030facc
Umur Gürgöze <86147088+umurgurgoze@users.noreply.github.com>
2022-10-18 15:41:06 +0300

    Complete
    Jquery training files

---
+++ JQuery.Training/JQuery.Training/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JQuery.Training.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
